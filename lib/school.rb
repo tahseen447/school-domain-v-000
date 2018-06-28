1d83d589a6cd000e9a39f52e6943dc9f0c81df81
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-28 22:21:21 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,8 +13,8 @@ class School
   end
 
   def add_student(student_name, grade)
-    if @roster.has_key?(grade)
-      #@roster[grade] = []
+    if !@roster.has_key?(grade)
+      @roster[grade] = []
     end
       @roster[grade] << student_name
   end
